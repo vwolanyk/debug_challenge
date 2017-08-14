56e9e79e71415b446a136446411d118fa565fdb5
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-08-14 13:32:32 -0400

    Add show method to category api controller

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -3,4 +3,9 @@ class Api::CategoriesController < ApplicationController
     @categories = Category.all
     render json: @categories, each_serializer: CategorySerializer
   end
+
+  def show
+    @category = Category.find(params[:id])
+    render json: @category, each_serializer: CategorySerializer
+  end
 end
